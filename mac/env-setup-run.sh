9e4cd9485c7a9ff86891b861ef2494eea4efa923
Samiran Saha <samiran.s89@gmail.com>
2025-11-24 11:56:22 +0530

    py 3.12

--- mac/env-setup-run.sh
+++ mac/env-setup-run.sh
@@ -263,7 +263,7 @@ setup_app_python() {
     detect_setup_python_env
     
     # Install dependencies
-    pip install -r requirements.txt >> "$NOW_RUN_LOG_FILE" 2>&1
+    pip3 install -r requirements.txt >> "$NOW_RUN_LOG_FILE" 2>&1
     log_success "Dependencies installed"
     
     local app_url=$BROWSERSTACK_APP
